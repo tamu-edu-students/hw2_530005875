8a64bcafecea0a03035d6110f0e13e53dc0157cc
Shubham026 <shubhamagarwal026@gmail.com>
2022-10-31 19:31:42 -0500

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'The Prestige', :rating => 'PG-13',
+    :release_date => '20-Oct-2006'}    
 ]
 
 more_movies.each do |movie|
